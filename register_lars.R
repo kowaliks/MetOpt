17e45dc9ce456196b3d26c3785bcf7a17547f062
kowaliks <kowal96@gmail.com>
2020-01-10 16:00:25 +0100

    project separated to 2 files

---
+++ register_lars.R
@@ -0,0 +1,56 @@
+Sys.setenv(ROI_LOAD_PLUGINS = "FALSE")
+library(ROI)
+
+library(dplyr)
+library(slam)
+library(lars)
+library(expm)
+
+
+lars_signature <- ROI_plugin_make_signature(objective = "Q", constraints = "L",
+                                            types = c("C"), bounds = c("X"), maximum = c(FALSE))
+
+ROI_plugin_add_status_code_to_db("lars", 0L, "OPT", "Solution is optimal", roi_code = 0L)
+
+
+# ROI wrapper on lars
+xyl_from_op <- function(op) {
+  constraints_matrix <- op$constraints$L
+  count_cols_nonzero_values <- table(constraints_matrix$j)
+  gamma_cols <- (as.data.frame(count_cols_nonzero_values) %>% filter(Freq == 1))$Var1 #cols with one value
+  
+  m <- length(gamma_cols)
+  n <- (NCOL(constraints_matrix) - m)/2
+  
+  #if(constraints_matrix[1][gamma_cols[1]] != 1) {
+  #  stop("unsupported problem representation")
+  #}
+  
+  x <- as.matrix(constraints_matrix)[1:m, 1:n]
+  y <- op$constraints$rhs[1:m]
+  
+  lambda <- op$objective$L[m+2*n]
+  
+  list(x, y, lambda)
+}
+
+calc_optimum <- function(x, y, lambda, b) {
+  1/2 * norm(y - x %*% b, "2") ^ 2 + lambda * norm(b, "1")
+}
+
+# solver method for ROI
+lars_solve_op <- function(op, control) {
+  xyl <- xyl_from_op(op)
+  x <- xyl[[1]]
+  y <- xyl[[2]]
+  lambda <- xyl[[3]]
+  
+  out <- lars(x, y, normalize=FALSE, intercept=FALSE, type="lasso")
+  b <- coef(out, s=lambda, mode="lambda")
+  res <- ROI_plugin_canonicalize_solution(solution = b, optimum = calc_optimum(x, y, lambda, as.matrix(b)),
+                                          status = 0L, solver = "lars",
+                                          message = out)
+  res
+}
+
+ROI_plugin_register_solver_method(lars_signature, "lars_final_2", lars_solve_op)
--- roi.R
+++ roi.R
@@ -10,9 +10,6 @@ library(dplyr)
 library(slam)
 library(lars)
 
-y <- longley[,1]
-x <- as.matrix(longley[,-1])
-
 # diagonal bind
 dbind <- function(...) {
   .dbind <- function(x, y) {
@@ -52,49 +49,11 @@ qp_lasso <- function(x, y, lambda) {
   op
 }
 
+
+y <- longley[,1]
+x <- as.matrix(longley[,-1])
+
+
 op <- qp_lasso(x, y, 2)
-# (qp0 <- ROI_solve(op, "qpoases"))
-
-
-# ROI wrapper on lars
-xyl_from_op <- function(op) {
-  constraints_matrix <- op$constraints$L
-  count_cols_nonzero_values <- table(constraints_matrix$j)
-  gamma_cols <- (as.data.frame(count_cols_nonzero_values) %>% filter(Freq == 1))$Var1 #cols with one value
-  
-  m <- length(gamma_cols)
-  n <- (NCOL(constraints_matrix) - m)/2
-  
-  #if(constraints_matrix[1][gamma_cols[1]] != 1) {
-  #  stop("unsupported problem representation")
-  #}
-  
-  x <- as.matrix(constraints_matrix)[1:m, 1:n]
-  y <- op$constraints$rhs[1:m]
-  
-  lambda <- op$objective$L[m+2*n]
-  
-  list(x, y, lambda)
-}
-
-
-
-lars_signature <- ROI_plugin_make_signature(objective = "Q", constraints = "L",
-                                            types = c("C"), bounds = c("X"), maximum = c(FALSE))
-
-ROI_plugin_add_status_code_to_db("lars", 0L, "OPT", "Solution is optimal", roi_code = 0L)
-
-lars_solve_op <- function(op, control) {
-  xyl <- xyl_from_op(op)
-  x <- xyl[[1]]
-  y <- xyl[[2]]
-  lambda <- xyl[[3]]
-  
-  out <- lars(x, y, normalize=FALSE, intercept=FALSE, type="lasso")
-  res <- ROI_plugin_canonicalize_solution(solution = coef(out, s=lambda, mode="lambda"),
-                                   optimum = -1, status = 0L, solver = "lars",
-                                   message = out)
-  res
-}
-
-ROI_plugin_register_solver_method(lars_signature, "lars", lars_solve_op)
+qp0 <- ROI_solve(op, "lars_final_2")
+solution(qp0)
