496ac68322920b0bf622c5dba2f5882c2bb806c3
kowaliks <kowal96@gmail.com>
2020-01-26 22:32:17 +0100

    one more dataset source

--- read_data.R
+++ read_data.R
@@ -1,3 +1,5 @@
+# https://archive.ics.uci.edu/ml/datasets.php?format=&task=reg&att=&area=&numAtt=&numIns=&type=&sort=instDown&view=table
+
 # https://archive.ics.uci.edu/ml/datasets/YearPredictionMSD#
 music_df <- read.csv("data/YearPredictionMSD.txt", header=FALSE)
 music_matrix <- as.matrix(music_df)
